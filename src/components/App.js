c05baa9b048a5434ff3da3b8d901f340b1cca72f
Justin <justinagui988@gmail.com>
2025-05-15 10:31:22 +0300

    render projects using ProjectLists and ProjectItems

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Home from "./Home";
 import About from "./About";
 import ProjectList from "./ProjectList";
 import user from "../data/user";
+import projects from "../data/projects"; // Import projects separately
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <NavBar />
       <Home username={user.name} city={user.city} color={user.color} />
       <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <ProjectList projects={projects} /> {/* Pass in the correct projects array */}
     </div>
   );
 }
---
+++ src/data/projects.js
@@ -0,0 +1,16 @@
+const projects = [
+  {
+    id: 1,
+    name: "Project 1",
+    about: "A React project",
+    technologies: ["React", "CSS"]
+  },
+  {
+    id: 2,
+    name: "Project 2",
+    about: "Another cool project",
+    technologies: ["JavaScript", "HTML"]
+  }
+];
+
+export default projects;
